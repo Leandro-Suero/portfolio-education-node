91e4bf60b4772bcda30675e9c69d687f98d90edd
Leandro-Suero <ing.leandrosuero@gmail.com>
2020-10-22 10:59:05 -0300

    configuring DB connection for heroku postgres deploy

--- src/database/database.js
+++ src/database/database.js
@@ -1,24 +1,30 @@
 import Sequelize from "sequelize";
 
-export const sequelize = new Sequelize(
-  process.env.DB_DATABASE_NAME,
-  process.env.DB_USER_NAME,
-  process.env.DB_USER_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    // dialectOptions: {
-    //   dateStrings: true,
-    //   typeCast: true,
-    //   useUTC: false,
-    // },
-    pool: {
-      max: 5,
-      min: 0,
-      require: 30000,
-      idle: 10000,
-    },
-    logging: false, //remove this to get feedback
-    timezone: process.env.DB_TIMEZONE,
-  }
-);
+const sequelizeOptions = {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  pool: {
+    max: 5,
+    min: 0,
+    require: 30000,
+    idle: 10000,
+  },
+  logging: false, //remove this to get feedback
+  timezone: process.env.DB_TIMEZONE,
+};
+
+let sequelize = {};
+if (process.env.DATABASE_URL) {
+  //heroku postgresql config
+  sequelize = new Sequelize(process.env.DATABASE_URL, sequelizeOptions);
+} else {
+  //local env postgres, dev
+  sequelize = new Sequelize(
+    process.env.DB_DATABASE_NAME,
+    process.env.DB_USER_NAME,
+    process.env.DB_USER_PASSWORD,
+    sequelizeOptions
+  );
+}
+
+export { sequelize };
